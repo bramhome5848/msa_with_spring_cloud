a419fb8ace7587990447363fc4f7da00b3b613ea
이경진 <bramhome5848@gmail.com>
2021-04-12 18:40:36 +0900

    20210412 Netflix Zuul - 프로젝트 생성

---
+++ zuul-service/src/test/java/com/lkj/zuulservice/ZuulServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lkj.zuulservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
