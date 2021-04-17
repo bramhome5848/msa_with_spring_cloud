e43a351ab693146e2adbeb9d09b68e6576c1d5a8
이경진 <bramhome5848@gmail.com>
2021-04-17 20:31:22 +0900

    20210417 Spring Cloud Gateway - 프로젝트 생성

---
+++ apigateway-service/src/test/java/com/lkj/apigatewayservice/ApigatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lkj.apigatewayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApigatewayServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
