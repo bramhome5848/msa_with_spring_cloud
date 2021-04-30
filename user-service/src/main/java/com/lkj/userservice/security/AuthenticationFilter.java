d7ac759a4691356fdf487dea0405b742f5b6762b
이경진 <bramhome5848@gmail.com>
2021-04-30 16:04:39 +0900

    20210430 User Microservice - 로그인 성공 처리

--- user-service/src/main/java/com/lkj/userservice/security/AuthenticationFilter.java
+++ user-service/src/main/java/com/lkj/userservice/security/AuthenticationFilter.java
@@ -1,11 +1,18 @@
 package com.lkj.userservice.security;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.lkj.userservice.dto.UserDto;
+import com.lkj.userservice.service.UserService;
 import com.lkj.userservice.vo.RequestLogin;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.stereotype.Component;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
@@ -14,13 +21,25 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.ArrayList;
 
+@Slf4j
 public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+    private final UserService userService;
+    private final Environment env;
+
+    public AuthenticationFilter(AuthenticationManager authenticationManager,
+                                UserService userService,
+                                Environment env) {
+        super.setAuthenticationManager(authenticationManager);
+        this.userService = userService;
+        this.env = env;
+    }
 
     @Override
     public Authentication attemptAuthentication(HttpServletRequest request,
                                                 HttpServletResponse response) throws AuthenticationException {
         try {
-            //post 전달은 request param 으로 받을 수 없기 때문에 -> inputstream 으로
+            //post 전달은 request param 으로 받을 수 없기 때문에 -> inputStream 으로
             RequestLogin creds = new ObjectMapper().readValue(request.getInputStream(), RequestLogin.class);
 
             //email, password, 권한에 대한 매개변수
@@ -41,6 +60,9 @@ public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
                                             HttpServletResponse response,
                                             FilterChain chain,
                                             Authentication authResult) throws IOException, ServletException {
-        //super.successfulAuthentication(request, response, chain, authResult);
+        String userName = ((User)authResult.getPrincipal()).getUsername();
+        UserDto userDetails = userService.getUserDetailsByEmail(userName);
+        log.info(userDetails.getEmail());
+        log.info(userDetails.getName());
     }
 }
